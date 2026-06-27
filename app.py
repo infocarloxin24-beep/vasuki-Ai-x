09023b7c3847a74bb0e090b72adafbc4495a8164
infocarloxin24-beep <infocarloxin24@gmail.com>
2026-06-27 12:46:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -352,7 +352,7 @@ with tab1:
     )
 
     username = st.text_input(f"{platform} Username / Profile Link:", placeholder="@username or paste profile URL")
-    scan_mode = st.radio("Scan Mode:", ["Auto - X /", "Manual -khud bharo"])
+    scan_mode = st.radio("Scan Mode:", ["Auto - X Nitter se data lao/", "Manual -khud bharo"])
 
     is_verified = False
     tweet_count = 0
