63bc98309215712cb23929ed5c20265df6eb677d
infocarloxin24-beep <infocarloxin24@gmail.com>
2026-06-29 11:44:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -974,6 +974,6 @@ with col_right:
 
 st.markdown("---")
 st.markdown(
-    "<div style='text-align: center; color: #666; padding: 20px 0; font-size: 14px;'>🐍 Version Vasuki Ai 4.0 - Bot Detector | Made in India | © 2026 All Rights Reserved</div>",
+    "<div style='text-align: center; color: #666; padding: 20px 0; font-size: 14px;'> Version 2 ScanX.Ai - Bot Detector | Made in India | © 2026 All Rights Reserved</div>",
     unsafe_allow_html=True
 )
