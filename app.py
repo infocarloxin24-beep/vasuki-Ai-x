10fffa663bd795e55e284ff50009335a4b9903d0
infocarloxin24-beep <infocarloxin24@gmail.com>
2026-07-04 11:30:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.switch_page("pages/ev_shield_final.py")
+st.switch_page("pages/01_ev_shield.py")
 import streamlit as st
 from analysis import run_all_analysis, init_sidebar_history, show_sidebar_share
 
