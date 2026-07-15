f7267f06c1c30258475c8b6114f904d1cebd63e1
infocarloxin24-beep <294381023+infocarloxin24-beep@users.noreply.github.com>
2026-07-15 11:09:18 +0530

    Update clyess.py

--- clyess.py
+++ clyess.py
@@ -8,9 +8,9 @@ from streamlit_mic_recorder import mic_recorder
 st.set_page_config(page_title="ClyxessChat AI", page_icon="💬", layout="centered", initial_sidebar_state="collapsed")
 
 # Absolute Fixed Infrastructure Credentials - Encrypted Backend Storage
-HIDDEN_GROQ_API_KEY = "GROQ_API_KEY"
-SUPABASE_URL = "https://anudnaqtgzkjciplrrgg.supabase.co/rest/v1/"
-SUPABASE_KEY = "sb_publishable_9kgpcnkITeh-kTXyRFJg6A_qLLouJmn"
+HIDDEN_GROQ_API_KEY = st.secrets["GROQ_API_KEY"
+SUPABASE_URL = st.secrets["SUPABASE_URL"
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"
 
 # 2026 Fully Active Groq Production Model ID
 ACTIVE_GROQ_MODEL = "qwen-2.5-coder-32b"
