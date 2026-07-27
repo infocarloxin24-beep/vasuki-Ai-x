6063d55ef08b5e32a5efdeab4431a4b017c22a53
infocarloxin24-beep <294381023+infocarloxin24-beep@users.noreply.github.com>
2026-07-27 18:19:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -338,7 +338,7 @@ supabase: Client = create_client(url, key)
 
 st.set_page_config(page_title="HumbotiX - Bot Detector", page_icon="Ḫ", layout="wide")
 st.title("HumbotiX Ai - Universal Bot Detector")
-st.caption("Global Multi Social-Platform Account & Text Scanner | Powered by AI")
+st.caption("Global Ai PoweredForensic Platform For Multiple Social Media Account & Bot Detection | Powered by AI")
 
 st.info("⚠️ Disclaimer: This tool provides an AI-assisted probability estimate and should not be treated as definitive proof.")
 
